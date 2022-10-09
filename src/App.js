48c7cfaceb0e11c5ed925ad0bdfecc4a870f6de2
gaunt0066 <gaunt0066@gmail.com>
2022-10-09 21:46:17 +0300

    made card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const[item,setItem]=useState(false)
+  const[hide,setHide]=useState(false)
+const deleteEvent=()=>{
+setItem(false)
+
+
+}
+
+  const buyEvent=()=>{
+  setItem(true)
+  
+   }
+
+
+const hidden=()=>{
+setHide(true)
+}
+const hideblock = ()=>{
+setHide(false)
+}
+
   return (
+
+
+  
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+      <div className='card'>
+        <img className='img' src=
+          "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/fdeb34c8-a6bf-4530-9145-b6cfea5dd963/air-force-1-07-lv8-1-mens-shoes-x0bMdL.png" alt=""
+        />
+        <div className='main-items'>
+          <div className='item-name'> Nike Sneaker</div>
+          <button className='btn1' onClick={hidden} >Детали</button>
+       
+       
+          {hide===true ? <div className='details-popup'>
+           <button className='hide' onClick={hideblock} >x</button>
+          <div className='hiden'>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel rerum, vitae delectus fugiat nisi opti'
+           </div>
+            </div>: false}
+        </div>
+        <h3 className='price'>$120</h3>
+        <div className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam laudantium sit suscipit ut. Saepe, eius omni</div>
+        <div className='buttons'>
+        <button className={item===false ? 'add' : 'disabled add'} disabled={item} onClick={buyEvent}>{item===false ?  'Добавить  в корзину':'в корзине'  }</button>
+        {item &&  <button onClick={deleteEvent} className='delete'>удалить из корзины</button>}
+        </div>
+      </div>
+      
+      
+    
     </div>
   );
 }
